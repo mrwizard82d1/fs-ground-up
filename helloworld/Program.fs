e866a0c27a7c8ed3654c02cd5297e1b1edb35df6
Larry Jones <mrwizard82d1@gmail.com>
2026-01-09 17:57:49 -0600

    Obligatory "Hello, World from F#" program

---
+++ helloworld/Program.fs
@@ -0,0 +1,8 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
