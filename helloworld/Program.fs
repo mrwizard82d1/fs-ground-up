c5b209f8f1bc970e7980b0844bf8860bdff6ace3
Larry Jones <mrwizard82d1@gmail.com>
2026-01-10 12:05:36 -0600

    User iterator to loop (much preferred)

--- helloworld/Program.fs
+++ helloworld/Program.fs
@@ -4,8 +4,7 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    for i in 0..argv.Length - 1 do
-        let person = argv.[i]
+    for person in argv do
         printfn "Hello, %s, from my F# program!" person
     printfn "Nice to meet you."
     0
