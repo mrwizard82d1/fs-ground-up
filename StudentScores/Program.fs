fe613dbc845a4c73fc48d767183722c2c82dc0bd
Larry Jones <mrwizard82d1@gmail.com>
2026-01-10 20:33:44 -0600

    Extract printing summary to function

--- StudentScores/Program.fs
+++ StudentScores/Program.fs
@@ -1,15 +1,19 @@
 ﻿open System
 open System.IO
 
+let summarize_file filePath =
+    let rows = File.ReadAllLines filePath
+    let studentCount = (rows |> Array.length) - 1
+    printfn "Found %i students" studentCount
+
+
 [<EntryPoint>]
 let main argv =
     if argv.Length = 1 then
         let filePath = argv.[0]
         if File.Exists filePath then
             printfn "Processing %s" filePath
-            let rows = File.ReadAllLines filePath
-            let studentCount = (rows |> Array.length) - 1
-            printfn "Found %i students" studentCount
+            summarize_file filePath
             0
         else
             printfn "Supplied file does not exist %s" filePath
