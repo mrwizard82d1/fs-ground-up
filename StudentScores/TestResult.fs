240422514790005c4e8878dee07aea23fbcb218e
Larry Jones <mrwizard82d1@gmail.com>
2026-01-11 20:30:04 -0600

    New test result: Voided
    Handled the same as Excused

--- StudentScores/TestResult.fs
+++ StudentScores/TestResult.fs
@@ -3,6 +3,7 @@ namespace StudentScores
 type TestResult =
     | Absent
     | Excused
+    | Voided
     | Scored of float
     
 module TestResult =
@@ -12,6 +13,8 @@ module TestResult =
             Absent
         elif s = "E" then
             Excused
+        elif s = "V" then
+            Voided
         else
             let value = s |> float
             Scored value
@@ -19,6 +22,8 @@ module TestResult =
     let tryEffectiveScore (testResult : TestResult) =
         match testResult with
             | Absent -> Some 0.0
-            | Excused -> None
+            // Map both an Excused result and a Voided result to `None`
+            | Excused
+            | Voided -> None
             | Scored score -> Some score
             
