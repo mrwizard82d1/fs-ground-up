de39893824e7074a3915fc9f00ed0874f96f0d91
Larry Jones <mrwizard82d1@gmail.com>
2026-01-12 21:40:24 -0600

    Do we support String instances? (Yes)

--- GenericPoints/Program.fs
+++ GenericPoints/Program.fs
@@ -8,11 +8,15 @@ let main _argv =
     printfn "pFloat1 %A pFloat2: %A" pFloat1 pFloat2
     
     let pInt1 = { X = 1; Y = 2 }
-    let pInt2 = pInt1 |> Point.moveBy 3 4
-    printfn "pInt1 %A pInt2: %A" pInt1 pInt2
     
     let pSingle1 = { X = 1.0f; Y = 2.0f }
     let pSingle2 = pSingle1 |> Point.moveBy 3.0f 4.0f
     printfn "pSingle1 %A pSingle2: %A" pSingle1 pSingle2
+    let pInt2 = pInt1 |> Point.moveBy 3 4
+    printfn "pInt1 %A pInt2: %A" pInt1 pInt2
+    
+    let pString1 = { X = "a"; Y = "b" }
+    let pString2 = pString1 |> Point.moveBy "c" "d"
+    printfn "pString1 %A pString2: %A" pString1 pString2
     
     0
