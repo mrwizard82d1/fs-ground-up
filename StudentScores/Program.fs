0242edb8f8bc385277190dc7c908831240acc082
Larry Jones <mrwizard82d1@gmail.com>
2026-01-12 18:22:17 -0600

    Exercises in handling exceptions

--- StudentScores/Program.fs
+++ StudentScores/Program.fs
@@ -14,13 +14,20 @@ let main argv =
                 0
             with
                 // Capture the specific exception using `as e` clause
-                | :? FormatException as e ->
+                | :? FormatException as fe ->
                     printfn "The file was not in the expected format."
-                    printfn "Details: %s" e.Message
-                    1
+                    printfn "Details: %s" fe.Message
+                    3
+                | :? IOException as ioe ->
+                    printfn "Could not open the file %s." filePath
+                    printfn "Details: %s" ioe.Message
+                    4
+                | ex ->
+                    printfn "Unhandled exception: %s" ex.Message
+                    5
         else
             printfn "Supplied file does not exist %s" filePath
             2
     else
         printfn "Please supply a filename"
-        3
+        1
