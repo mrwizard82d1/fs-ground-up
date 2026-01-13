983f1917d473c87c8a883236e5c52adca555d278
Larry Jones <mrwizard82d1@gmail.com>
2026-01-12 21:32:58 -0600

    Implement GenericPoints module

---
+++ GenericPoints/Program.fs
@@ -0,0 +1,18 @@
+﻿open GenericPoints
+
+[<EntryPoint>]
+let main _argv =
+    
+    let pFloat1 = { X = 1.0; Y = 2.0 }
+    let pFloat2 = pFloat1 |> Point.moveBy 3.0 4.0
+    printfn "pFloat1 %A pFloat2: %A" pFloat1 pFloat2
+    
+    let pInt1 = { X = 1; Y = 2 }
+    let pInt2 = pInt1 |> Point.moveBy 3 4
+    printfn "pInt1 %A pInt2: %A" pInt1 pInt2
+    
+    let pSingle1 = { X = 1.0f; Y = 2.0f }
+    let pSingle2 = pSingle1 |> Point.moveBy 3.0f 4.0f
+    printfn "pSingle1 %A pSingle2: %A" pSingle1 pSingle2
+    
+    0
