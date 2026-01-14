22a05e6b79b84be67f7b23ecea067f5b3a9e8650
Larry Jones <mrwizard82d1@gmail.com>
2026-01-14 16:42:30 -0600

    Sum squares of integers 1..1000 using Array.init

--- ArrayCreation/Program.fs
+++ ArrayCreation/Program.fs
@@ -8,5 +8,11 @@ let main _argv =
     let numbers = Array.init 5 (fun i -> pown 2 i)
 
     printfn $"%A{numbers}"
+    
+    let total =
+        Array.init 1000 (fun i -> (i + 1) * (i * 1))
+        |> Array.sum
+        
+    printfn $"%d{total}"
 
     0
