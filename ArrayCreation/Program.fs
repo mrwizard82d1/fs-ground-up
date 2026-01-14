b2dc0a456b52848199c9c8a6ff5cfe33102550be
Larry Jones <mrwizard82d1@gmail.com>
2026-01-14 16:38:21 -0600

    Other ways to create Arrays: `Array.init`

--- ArrayCreation/Program.fs
+++ ArrayCreation/Program.fs
@@ -4,34 +4,9 @@
 [<EntryPoint>]
 let main _argv =
     
-    let isEven n = n % 2 = 0
-    
-    let todayIsThursday() =
-        DateTime.Now.DayOfWeek = DayOfWeek.Thursday
-    
-    // Initialize an array using an "Array comprehension"
-    let numbers = [|
-        // This form yields **multiple results**
-        // - 42 (if today is Thursday)
-        // - The squares of integers 0..9 (includes 0 but not 9)
-        // - 999
-        if todayIsThursday () then 42
-        // if not (todayIsThursday ()) then 42
-        for i in 0..9 do
-            let x = i * i
-            if x |> isEven then
-                x // This is the "implicit yield" form of `for...in...do`
-        999
-    |]
+    // Other ways to initialize Arrays
+    let numbers = Array.init 5 (fun i -> pown 2 i)
 
     printfn $"%A{numbers}"
-    
-    // Calculate the sum of the squares of integers between
-    // 1 and 1000, inclusive.
-    let total =
-        [| for i in 1..1000 -> i * i |]
-        |> Array.sum
-    
-    printfn $"%d{total}"
 
     0
