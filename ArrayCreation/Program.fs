2dec3c76d7efc5c08c2efdba2eafcca3e0e685e7
Larry Jones <mrwizard82d1@gmail.com>
2026-01-14 16:47:54 -0600

    Remember that Arrays are always **mutable**
    This characteristic is useful for low-level .NET libraries that will
    fill an Array. For example, using the .NET stream function,
    `Read(Byte[], Int32, Int32)`.

--- ArrayCreation/Program.fs
+++ ArrayCreation/Program.fs
@@ -6,6 +6,9 @@ let main _argv =
     
     // Other ways to initialize Arrays
     let initiallyZeros = Array.zeroCreate<int> 10
+    
+    // NOTE: items of type `Array` are **mutable** by default
+    initiallyZeros.[0] <- 42
 
     printfn $"%A{initiallyZeros}"
 
