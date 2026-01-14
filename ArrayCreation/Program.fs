593fb1d73c2996c8bfdd8bf15ff402a9e1b4bdab
Larry Jones <mrwizard82d1@gmail.com>
2026-01-14 16:11:17 -0600

    Use `for..in..do`
    I'm still quite confused about the difference between `for..in..->` and
    `for..in..do`. Additionally, Scott Wlaschin recommends **not** using any
    of a number of "typical" expression:
    
    - if-then-else
    - for (in all its forms)

--- ArrayCreation/Program.fs
+++ ArrayCreation/Program.fs
@@ -1,11 +1,14 @@
-﻿open System
-
-[<EntryPoint>]
+﻿[<EntryPoint>]
 let main _argv =
+    
+    let isEven n = n % 2 = 0
     
     // Initialize an array using an expression
     let numbers = [|
-        for i in 0..4 -> pown 2 i
+        for i in 0..9 do
+            let x = i * i
+            if x |> isEven then
+                x // This is the "implicit yield" form of `for...in...do`
     |]
 
     printfn $"%A{numbers}"
