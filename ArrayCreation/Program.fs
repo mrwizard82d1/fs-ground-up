5930566e2cae44eb2d7c5c7a1acd402d2b15dac7
Larry Jones <mrwizard82d1@gmail.com>
2026-01-14 16:16:50 -0600

    Yielding values before and after for..in

--- ArrayCreation/Program.fs
+++ ArrayCreation/Program.fs
@@ -5,10 +5,16 @@ let main _argv =
     
     // Initialize an array using an expression
     let numbers = [|
+        // This form yields **multiple results**
+        // - 42
+        // - The squares of integers 0..9 (includes 0 but not 9)
+        // - 999
+        42
         for i in 0..9 do
             let x = i * i
             if x |> isEven then
                 x // This is the "implicit yield" form of `for...in...do`
+        999
     |]
 
     printfn $"%A{numbers}"
