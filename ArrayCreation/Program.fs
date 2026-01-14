d653bef40b819206b7adb43d5aae8bc1bbc173b6
Larry Jones <mrwizard82d1@gmail.com>
2026-01-14 16:20:26 -0600

    Using an Array comprehension

--- ArrayCreation/Program.fs
+++ ArrayCreation/Program.fs
@@ -1,15 +1,22 @@
-﻿[<EntryPoint>]
+﻿open System
+
+
+[<EntryPoint>]
 let main _argv =
     
     let isEven n = n % 2 = 0
     
+    let todayIsThursday() =
+        DateTime.Now.DayOfWeek = DayOfWeek.Thursday
+    
     // Initialize an array using an expression
     let numbers = [|
         // This form yields **multiple results**
-        // - 42
+        // - 42 (if today is Thursday)
         // - The squares of integers 0..9 (includes 0 but not 9)
         // - 999
-        42
+        if todayIsThursday () then 42
+        // if not (todayIsThursday ()) then 42
         for i in 0..9 do
             let x = i * i
             if x |> isEven then
