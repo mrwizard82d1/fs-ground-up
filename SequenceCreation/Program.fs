5133ef4d787d41dcea6e5a6e61dab3dddddd352a
Larry Jones <mrwizard82d1@gmail.com>
2026-01-15 15:07:37 -0600

    Hmm.. Something is not right with my solution
    I print out a sequence:
    
    0, 1, 1, 1, ...
    
    But when I perform the calculation mentally, I think everything is
    correct. I'm uncertain what I've done wrong. Time to move on... Sigh...

--- SequenceCreation/Program.fs
+++ SequenceCreation/Program.fs
@@ -2,33 +2,38 @@
 
 module MathSequence =
     
+    type State = {
+        n: int
+        pSub1: int
+        pSub2: int
+    }
+    
     let pell =
-        // (index of the sequence item to calculate;
-        //  p-sub-n-minus-1; p-sub-n-minus-2)
-        (0, 0, 0)
-        |> Seq.unfold (fun (n, pn2, pn1) ->
+        let initialState = { n = 0; pSub1 = 0; pSub2 = 0 }
+        initialState
+        |> Seq.unfold (fun state ->
             // Calculate the next item in the sequence
-            let pn =
-                match n with
+            let pSubN =
+                match state.n with
                 | 0 | 1 ->
-                    n
+                    state.n
                 | _ ->
-                    2 * pn1 + pn2
+                    2 * state.pSub1 + state.pSub2
             // Increment the index of the sequence item 
-            let n' = n + 1
+            let n' = state.n + 1
             // Return the next item in the sequence (`pn` or p-sub-n)
             // and a tuple consisting of
             // - The index of the next item in the sequence
             // - The previous item in the sequence
             // - The (calculated) next item in the sequence
-            Some (pn, (n', pn1, pn)))
+            Some (pSubN, { n = n'; pSub1 = state.pSub1; pSub2 = pSubN }))
 
 [<EntryPoint>]
 let main _argv = 
 
     MathSequence.pell
     |> Seq.truncate 10
-    |> Seq.iter (fun x -> printf $"%i{x}, ")
+    |> Seq.iter (fun x -> printf $"%A{x}, ")
     
     printfn "..."
     
