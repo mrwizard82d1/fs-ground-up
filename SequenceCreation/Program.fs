9e2a0633d2332fd639d71cb99726f94f42804a09
Larry Jones <mrwizard82d1@gmail.com>
2026-01-15 14:24:47 -0600

    Initialize an infinite sequence (and truncate it)

--- SequenceCreation/Program.fs
+++ SequenceCreation/Program.fs
@@ -3,13 +3,14 @@
 [<EntryPoint>]
 let main _argv = 
 
-    let total =
-        // seq { for i in 1..1000 -> i * i }
-        //
-        // Illustrate initializing a sequence (like `Array.init`)
-        Seq.init 1000 (fun i ->
+    let squares =
+        Seq.initInfinite (fun i ->
             let j = i + 1
             j * j)
+    let total =
+        squares
+        // Hmm... Why not `Seq.take`?
+        |> Seq.truncate 1000
         |> Seq.sum
 
     printfn $"The total is: %i{total}"
