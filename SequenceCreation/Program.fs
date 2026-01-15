d2e546bb4014ace9b912fa57b5074fc4c16c02d1
Larry Jones <mrwizard82d1@gmail.com>
2026-01-15 14:49:34 -0600

    Calculate the pell numbers
    A pell number is an item in an infinite sequence calculated by the
    recursion relationship:
    
    - p-sub-n = 0 if n = 0
    - p-sub-n = 1 if n = 1
    - p-sub-n = 2 * p-sub-n-minus-1 + p-sub-n-minus-2

--- SequenceCreation/Program.fs
+++ SequenceCreation/Program.fs
@@ -1,17 +1,35 @@
 ﻿open System
 
-module Dates =
+module MathSequence =
     
-    let from (startDate: DateTime) =
-        Seq.initInfinite (fun i -> startDate.AddDays(float i))
+    let pell =
+        // (index of the sequence item to calculate;
+        //  p-sub-n-minus-1; p-sub-n-minus-2)
+        (0, 0, 0)
+        |> Seq.unfold (fun (n, pn2, pn1) ->
+            // Calculate the next item in the sequence
+            let pn =
+                match n with
+                | 0 | 1 ->
+                    n
+                | _ ->
+                    2 * pn1 + pn2
+            // Increment the index of the sequence item 
+            let n' = n + 1
+            // Return the next item in the sequence (`pn` or p-sub-n)
+            // and a tuple consisting of
+            // - The index of the next item in the sequence
+            // - The previous item in the sequence
+            // - The (calculated) next item in the sequence
+            Some (pn, (n', pn1, pn)))
 
 [<EntryPoint>]
 let main _argv = 
 
-    Dates.from DateTime.Now
-    |> Seq.filter (fun d -> d.Month = 1 && d.Day = 1)
-    // Only take 10 items from filtered sequence
+    MathSequence.pell
     |> Seq.truncate 10
-    |> Seq.iter (fun d -> printfn $"%i{d.Year} %s{d.DayOfWeek.ToString()}")
-
+    |> Seq.iter (fun x -> printf $"%i{x}, ")
+    
+    printfn "..."
+    
     0
