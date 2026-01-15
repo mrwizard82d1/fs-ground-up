3e899883d66f6899a261b53627734ba281011a76
Larry Jones <mrwizard82d1@gmail.com>
2026-01-15 14:18:26 -0600

    Create program for sequence creation

---
+++ SequenceCreation/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+[<EntryPoint>]
+let main _argv = 
+
+    let total =
+        seq { for i in 1..1000 -> i * i }
+        |> Seq.sum
+
+    printfn $"The total is: %i{total}"
+
+    0
