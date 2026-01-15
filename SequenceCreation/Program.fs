614ab26226777ae0148cad0906d84d2e7161503b
Larry Jones <mrwizard82d1@gmail.com>
2026-01-15 16:15:45 -0600

    Author's solution for 10 steps

--- SequenceCreation/Program.fs
+++ SequenceCreation/Program.fs
@@ -1,45 +1,33 @@
 ﻿open System
 
-module MathSequence =
+module Drunkard =
     
-    type State = {
-        n: int
-        pSub1: int
-        pSub2: int
+    let r = System.Random() // A random number generator
+    
+    // Returns a random integer between -1 and +1, inclusive.
+    let step() =
+        r.Next(-1, 2)
+        
+    type Position = {
+        X: int
+        Y: int
     }
     
-    let pell =
-        let initialState = { n = 0; pSub1 = 0; pSub2 = 0 }
-        initialState
-        |> Seq.unfold (fun state ->
-            // Calculate the next item in the sequence
-            let pSubN =
-                match state.n with
-                | 0 | 1 ->
-                    state.n
-                | _ ->
-                    2 * state.pSub1 + state.pSub2
-            // Increment the index of the sequence item 
-            let n' = state.n + 1
-            // Return the next item in the sequence (`pn` or p-sub-n)
-            // and a tuple consisting of
-            // - The index of the next item in the sequence
-            // - The previous item in the sequence
-            // - The (calculated) next item in the sequence
-            //
-            // Interesting... Somehow in my solution, I did not realize
-            // that the first item in original tuple was modeling
-            // p-sub-n-minus-2 and the second item was modeling
-            // p-sub-n-minus-1.
-            Some (pSubN, { n = n'; pSub2 = state.pSub1; pSub1 = pSubN }))
+    let walk =
+        { X = 0; Y = 0 }
+        |> Seq.unfold (fun position ->
+            let x' = position.X + step()
+            let y' = position.Y + step()
+            let position' = { X = x'; Y = y' }
+            Some(position', position'))
 
 [<EntryPoint>]
 let main _argv = 
 
-    MathSequence.pell
-    |> Seq.truncate 10
-    |> Seq.iter (fun x -> printf $"%A{x}, ")
+    printf "Drunkard's walk"
     
-    printfn "..."
+    Drunkard.walk
+    |> Seq.take 10
+    |> Seq.iter (fun p -> printfn $"X: %i{p.X} Y: %i{p.Y}")
     
     0
