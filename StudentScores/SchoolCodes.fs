6e45dc84ce58477be77c671d282a2ff6dc7306d8
Larry Jones <mrwizard82d1@gmail.com>
2026-01-15 17:09:21 -0600

    Everything compiles, but I not the expected output
    More work to do. Some items I know I need:
    
    - Supply the pathname of the school codes - school names file to the
      program
    - Summarize using both the data file and the school codes file

--- StudentScores/SchoolCodes.fs
+++ StudentScores/SchoolCodes.fs
@@ -3,17 +3,24 @@ namespace StudentScores
 module SchoolCodes =
     
     open System.IO
+    open System.Collections.Generic
 
     let load (filePath: string )=
-        File.ReadAllLines filePath
-        |> Seq.skip 1
-        |> Seq.map (fun row ->
-            // Limit elements read to 2 (makes input data format errors
-            // slightly more visible).
-            let elements = row.Split('\t', 2)
-            // Assumes the correct format (that is, can be converted
-            // to an `int`)
-            let id = elements.[0] |> int
-            let name = elements.[1]
-            // Return the `id` and `name` as a tuple
-            id, name)
+        // The value, `pairs`, refers to a sequence of .NET
+        // // `KeyValuePair` items
+        let pairs =
+            File.ReadAllLines filePath
+            |> Seq.skip 1
+            |> Seq.map (fun row ->
+                // Limit elements read to 2 (makes input data format
+                // errors slightly more visible).
+                let elements = row.Split('\t', 2)
+                // Assumes the correct format (that is, can be converted
+                // to an `int`)
+                let id = elements.[0] |> int
+                let name = elements.[1]
+                // Return the `id` and `name` as a tuple
+                KeyValuePair.Create(id, name))
+            
+        // Create a new .NET `Dictionary`
+        Dictionary<int, string>(pairs)
