25b55e4d9a0c2b3cf2403f2daa1f2e1e5572a1d9
Larry Jones <mrwizard82d1@gmail.com>
2026-01-15 17:28:47 -0600

    An alternative implementation of SchoolCodes.load

--- StudentScores/SchoolCodes.fs
+++ StudentScores/SchoolCodes.fs
@@ -6,24 +6,37 @@ module SchoolCodes =
     open System.Collections.Generic
 
     let load (filePath: string )=
-        // The value, `pairs`, refers to a sequence of .NET
-        // // `KeyValuePair` items
-        let pairs =
-            File.ReadAllLines filePath
-            |> Seq.skip 1
-            |> Seq.map (fun row ->
-                // Limit elements read to 2 (makes input data format
-                // errors slightly more visible).
-                let elements = row.Split('\t', 2)
-                // Assumes the correct format (that is, can be converted
-                // to an `int`)
-                let id = elements.[0] |> int
-                let name = elements.[1]
-                // Return the `id` and `name` as a tuple
-                KeyValuePair.Create(id, name))
-            
-        // Create a new .NET `Dictionary`
-        //
-        // The F# compiler can (often) infer the correct types to put
-        // in the dictionary.
-        Dictionary<_, _>(pairs)
+        // // The value, `pairs`, refers to a sequence of .NET
+        // // // `KeyValuePair` items
+        // let pairs =
+        //     File.ReadAllLines filePath
+        //     |> Seq.skip 1
+        //     |> Seq.map (fun row ->
+        //         // Limit elements read to 2 (makes input data format
+        //         // errors slightly more visible).
+        //         let elements = row.Split('\t', 2)
+        //         // Assumes the correct format (that is, can be converted
+        //         // to an `int`)
+        //         let id = elements.[0] |> int
+        //         let name = elements.[1]
+        //         // Return the `id` and `name` as a tuple
+        //         KeyValuePair.Create(id, name))
+        //     
+        // // Create a new .NET `Dictionary`
+        // //
+        // // The F# compiler can (often) infer the correct types to put
+        // // in the dictionary.
+        // Dictionary<_, _>(pairs)
+        
+        File.ReadAllLines filePath
+        |> Seq.skip 1
+        |> Seq.map (fun row ->
+            let elements = row.Split('\t', 2)
+            let id = elements.[0] |> int
+            let name = elements.[1]
+            id, name)
+        // This expression pipes the pairs into the `dict` function
+        // which can be used to create .NET `IDictionary`. Note that
+        // the function, `dict`, creates a `Dictionary` (that
+        // implements `IDictionary`)
+        |> dict
