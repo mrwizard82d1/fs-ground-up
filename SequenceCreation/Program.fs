455f94f63fa5e756c1a7ab3e5b598940f55a6939
Larry Jones <mrwizard82d1@gmail.com>
2026-01-15 14:21:54 -0600

    Using Seq.init to create a sequence

--- SequenceCreation/Program.fs
+++ SequenceCreation/Program.fs
@@ -4,7 +4,12 @@
 let main _argv = 
 
     let total =
-        seq { for i in 1..1000 -> i * i }
+        // seq { for i in 1..1000 -> i * i }
+        //
+        // Illustrate initializing a sequence (like `Array.init`)
+        Seq.init 1000 (fun i ->
+            let j = i + 1
+            j * j)
         |> Seq.sum
 
     printfn $"The total is: %i{total}"
