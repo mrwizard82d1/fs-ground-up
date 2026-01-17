f104343a9a69dbda7d0eca12754497ca8437b9b1
Larry Jones <mrwizard82d1@gmail.com>
2026-01-16 20:49:55 -0600

    Exercises 2 and 3 (Person and favorite colors)

--- Classes/ConsolePrompt.fs
+++ Classes/ConsolePrompt.fs
@@ -2,6 +2,21 @@ namespace Classes
 
 open System
 
+type Person(name : string, favoriteColorName: string) =
+    
+    do
+        if String.IsNullOrWhiteSpace(name) then
+            raise <| ArgumentException("Null or empty", "message")
+            
+    let favoriteColor =
+        if String.IsNullOrWhiteSpace(favoriteColorName) then
+            "(None)"
+        else
+            favoriteColorName
+            
+    member this.Description() =
+        sprintf $"Name: %s{name}, favorite color: %s{favoriteColor}"
+
 type ConsolePrompt(message: String, maxTries: int) =
     // Constructor (through `let trimmedMessage = ...`)
     do
--- Classes/Program.fs
+++ Classes/Program.fs
@@ -3,22 +3,15 @@ open Classes
 
 [<EntryPoint>]
 let main _argv =
-    // Invoking the `ConsolePrompt` constructor using `new`
-    // is **optional**. Note that instantiating this class
-    // **does not** actually prompt the user. The user is actually
-    // prompted to enter data when the `GetValue()` method is invoked.
     let namePrompt = ConsolePrompt("Please enter your name", 3)
-    
-    // Change (irritating) beeping on empty input.
-    namePrompt.BeepOnError <- false
-    
-    // Test out changing our Console color scheme
-    namePrompt.ColorScheme <- ConsoleColor.Cyan, ConsoleColor.DarkGray
-    
-    // Invokes the instance method, `GetValue()`. This method actually
-    // prompts the user for her name.
     let name = namePrompt.GetValue()
     
     printfn $"Hello %s{name}"
+    
+    let colorPrompt = ConsolePrompt("What is your favorite color (press `Enter` if you don't have one", 1)
+    let favoriteColor = colorPrompt.GetValue()
+    
+    let person = Person(name, favoriteColor)
+    printfn $"%s{person.Description()}"
     
     0
